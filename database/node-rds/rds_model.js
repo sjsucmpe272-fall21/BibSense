1fb3255ed82d71b0ee88533e476ed4d640dd0927
Dee <90799662+goomy241@users.noreply.github.com>
2021-11-27 20:09:56 -0800

    modify query syntax

--- database/node-rds/rds_model.js
+++ database/node-rds/rds_model.js
@@ -20,26 +20,14 @@ const client = new Client({
   port: 5432
 });
 
-// if(client.connection._events != null)
 client.connect(function(err) {
   client.query('SELECT NOW()', (err, res) => {
     if (err) console.log(err, res);
     console.log("Connected!");
-    // client.end();
   });
 });
 
-//   // callback
-// client.query('SELECT * FROM img_info;', (err, res) => {
-//   if (err) {
-//     console.log(err.stack)
-//   } else {
-//     console.log(res.rows)
-//   }
-// })
-
 const getImgs = () => {
-  // client.connect()
   return new Promise(function(resolve, reject) {
     client.query('SELECT * FROM img_info;', (error, results) => {
       if (error) {
@@ -50,29 +38,15 @@ const getImgs = () => {
   })
 }
 
-const query = 'SELECT s.img_name_s AS name, s.img_link FROM img_info AS i JOIN img_storage AS s ON s.img_name_s = i.img_name WHERE bib_number = $1;'
-// const bib = ['5074']
-
-// // callback
-// client.query(query, bib, (err, res) => {
-//   if (err) {
-//     console.log(err.stack)
-//   } else {
-//     console.log(res.rows)
-//   }
-// })
+const query = 'SELECT img_name AS name, img_link FROM img_info WHERE bib_number = $1;'
 
 const queryImgs = (req, res) => {
-  // console.log(`module bib: ` + req)
   const argument = parseInt(req, 10)
-  // console.log(`module 2: ` + argument)
   return new Promise(function(resolve, reject) {
-    // const bib = '3219'
     client.query(query, [argument], (error, results) => {
       if (error) {
         reject(error)
       }
-      // resolve(`image related to bibNumber: ${results.rows}`)
       resolve(results.rows);
     })
   })
