103ee862750b724cdd4ce1a49d8b5853ff5473a9
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2021-11-30 18:12:52 -0800

    changed background image

--- frontendFolder/src/App.js
+++ frontendFolder/src/App.js
@@ -15,15 +15,7 @@ class App extends Component {
       {/* <div >
         <Navigation/> 
         </div> */}
-   
-        {/* <div  style={{
-          backgroundImage: `url("http://localhost:3000/images/backgroundHome.jpg")` ,
-          backgroundRepeat: "no-repeat",
-          width: "100",
-          height: "150px",
-          backgroundSize: "cover",
-          position: "relative",
-          opacity: "0.9",}}  > */}
+        
              <div >        
       
           <Navigation/> 
